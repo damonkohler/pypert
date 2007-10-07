cb44022024a848c8606ddd70901e75445adfb53e
damonkohler <damonkohler@5fce3ec1-de3b-0410-9c76-5f19dca96c1e>
2007-10-07 03:40:15 +0000

    Initial import of PyPert and Xbox Live contib script.

---
+++ contrib/xbox_live.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+
+# The MIT License
+#
+# Copyright (c) 2007 Damon Kohler
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+"""Displays a list of your Xbox Live friends that are online.
+
+This script requires:
+  -BeautifulSoup, http://www.crummy.com/software/BeautifulSoup/
+  -mechanize, http://wwwsearch.sourceforge.net/mechanize/
+  -ClientForm, http://wwwsearch.sourceforge.net/ClientForm/
+
+"""
+from BeautifulSoup import BeautifulSoup
+from mechanize import Browser
+import pertelian
+import sys
+import time
+
+FRIEND_TABLE_CLASS = 'XbcProfileTable XbcFriendsListTable'
+GAMER_TAG_CLASS = 'XbcGamerTag'
+GAMER_PRESENCE_CLASS = 'XbcGamerPresence'
+
+
+def GetXboxLiveFriends(login, passwd):
+  """Return a list of tuples (gamer_tag, gamer_presence)."""
+  br = Browser()
+  br.open('http://live.xbox.com/en-US/profile/Friends.aspx')
+  br.select_form(name='f1')
+  br['login'] = login
+  br['passwd'] = passwd
+  br.submit()  # Submit login form.
+  br.select_form(name='fmHF')
+  response = br.submit()  # Submit redirect form.
+  friend_list = response.read()
+
+  soup = BeautifulSoup(friend_list)
+  friend_table = soup.find('table', {'class': FRIEND_TABLE_CLASS})
+  friends = []
+  for row in friend_table.contents[1:]:  # Skip header row.
+    gamer_tag = row.find('td', {'class': GAMER_TAG_CLASS})
+    gamer_tag = str(gamer_tag.find('a').contents[0])
+    gamer_presence = row.find('td', {'class': GAMER_PRESENCE_CLASS})
+    gamer_presence = str(gamer_presence.find('h4').contents[0])
+    friends.append((gamer_tag, gamer_presence))
+  return friends
+
+
+if __name__ == '__main__':
+  assert len(sys.argv) == 3, 'Usage: xbox_live.py login passwd'
+  login, passwd = sys.argv[1:2]
+  try:
+    p = pertelian.Pertelian()
+    while True:
+      friends = xbox_live.GetXboxLiveFriends()
+      online = []
+      for friend in friends:
+        if friend[1] == 'Online':
+          online.append(friend[0])
+      online.sort()
+      p.Clear()
+      if online:
+        p.Backlight(True)
+        p.Message(' '.join(online))
+      else:
+        p.Backlight(False)
+        p.Message('No friends online.')
+      time.sleep(30)
+  except KeyboardInterrupt:
+    pass
---
+++ pertelian.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python
+
+# The MIT License
+#
+# Copyright (c) 2007 Damon Kohler
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+"""Python interface to the Pertelian X2040 display."""
+
+import serial
+import struct
+import time
+
+CHARACTER_WIDTH = 20  # Number of characters that will fit on a line.
+
+
+class Pertelian(object):
+
+  def __init__(self, tty='/dev/ttyUSB0'):
+    self.ser = serial.Serial(tty)
+    self.ser.open()
+    self._Setup()
+
+  def __del__(self):
+    """Close the serial connection when this object is destroyed."""
+    self.ser.close()
+
+  def _Setup(self):
+    """Set up the display.
+
+    Function set with 8-bit data length, 2 lines, and 5x7 dot size.
+    Entry mode set; increment cursor direction; do not automatically shift.
+    Cursor/display shift; cursor move.
+    Display On; cursor off; do not blink.
+
+    """
+    for byte in (0x38, 0x06, 0x10, 0x0c, 0x01):
+      self._SendInstruction(byte)
+
+  def _SendBytes(self, bytes, delay=0.01, pack='B'):
+    """Send a stream of bytes to the Pertelian.
+    
+    Also, sleep for delay seconds between sending each byte.
+
+    """
+    for byte in struct.pack(pack * len(bytes), *bytes):
+      self.ser.write(byte)
+      time.sleep(delay)
+
+  def _SendInstruction(self, byte):
+    """Send an instruction byte to the Pertelian."""
+    self._SendBytes((0xfe, byte))
+
+  def Power(self, on):
+    """Turn the power on or off."""
+    if on:
+      self._SendInstruction(0x0c)
+    else:
+      self._SendInstruction(0x08)
+
+  def Backlight(self, on):
+    """Turn the backlight on or off."""
+    if on:
+      self._SendInstruction(0x03)
+    else:
+      self._SendInstruction(0x02)
+
+  def Clear(self):
+    """Clear the display."""
+    self._SendInstruction(0x01)
+
+  def Message(self, msg):
+    """Display a message."""
+    self._SendBytes(msg, pack='c')
+
+  def WrapMessage(self, msg):
+    """Wrap messages to display on the Pertelian.
+    
+    TODO(damonkohler): Add support for words longer than CHARACTER_WIDTH.
+
+    """
+    words = msg.split()
+    lines = []
+    line = words[0]
+    for word in words[1:]:
+      assert len(word) <= CHARACTER_WIDTH, 'Word too long.'
+      if len(line) + len(word) < CHARACTER_WIDTH:
+        line += ' ' + word
+      else:
+        padding = CHARACTER_WIDTH - len(line)
+        lines.append(line + ' ' * padding)
+        line = word
+    lines.append(line)
+    while len(lines) < 4:
+      lines.append('')  # Add any missing blank lines.
+    # The Pertelian displays lines a little out of order.
+    self.Message(lines[0])
+    self.Message(lines[2])
+    self.Message(lines[1])
+    self.Message(lines[3])
+
+
+if __name__ == '__main__':
+  p = Pertelian()
+  p.Message('Hello World!')
